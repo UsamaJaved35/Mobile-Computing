0a7191cbe80c33dbe0248779fc53d4d551e59111
Usama <usamamughal4949@gmail.com>
2022-05-28 21:35:42 +0500

    passing values between activities

--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -7,29 +7,31 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn=findViewById(R.id.button);
+        editText=findViewById(R.id.editText);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                composeEmail("bsef19m035@pucit.edu.pk","practice");
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                String message = editText.getText().toString();
+                intent.putExtra("ETM", message);
+                String staticString = "السلام علیکم";
+                intent.putExtra("SS", staticString);
+                intent.putExtra("abc", "It is another text");
+                        startActivity(intent);
+
             }
         });
 
     }
-    public void composeEmail(String address, String subject) {
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("*/*");
-        intent.putExtra(Intent.EXTRA_EMAIL, address);
-        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }  }
+
     }
