29e092d5c5eb941d279765bd46f558d0f926fe7e
Usama <usamamughal4949@gmail.com>
2022-05-29 16:13:55 +0500

    Sorting data in list

--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
@@ -22,15 +23,19 @@ public class MainActivity extends AppCompatActivity {
         editText=findViewById(R.id.editText);
         listView=findViewById(R.id.listView);
         ArrayList<String> friendList=new ArrayList<String>();
+        friendList.add("usama");
+        friendList.add("zaman");
+        friendList.add("ahmad");
+        friendList.add("osama");
         ArrayAdapter<String>arrayAdapter=new ArrayAdapter<String>(
                 this, android.R.layout.simple_list_item_1,friendList);
+        Collections.sort(friendList);
         listView.setAdapter(arrayAdapter);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                friendList.add(editText.getText().toString());
-                arrayAdapter.notifyDataSetChanged();
-            }});
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                friendList.add(editText.getText().toString());
+//                arrayAdapter.notifyDataSetChanged();
+//            }});
     }
-
     }
