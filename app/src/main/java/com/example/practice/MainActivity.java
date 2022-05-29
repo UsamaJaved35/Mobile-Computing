b7b219faac6677bbafaff69d31ad4b05842c87ab
Usama <usamamughal4949@gmail.com>
2022-05-29 15:51:12 +0500

    Implemanting List Views and ArrayList

--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -1,37 +1,33 @@
 package com.example.practice;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
     Button btn;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn=findViewById(R.id.button);
         editText=findViewById(R.id.editText);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                String message = editText.getText().toString();
-                intent.putExtra("ETM", message);
-                String staticString = "السلام علیکم";
-                intent.putExtra("SS", staticString);
-                intent.putExtra("abc", "It is another text");
-                        startActivity(intent);
-
-            }
-        });
-
+        listView=findViewById(R.id.listView);
+        ArrayList<String> friendList=new ArrayList<String>();
+        friendList.add("usama");
+        friendList.add("ahmad");
+        friendList.add("osama");
+        ArrayAdapter<String>arrayAdapter=new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1,friendList);
+        listView.setAdapter(arrayAdapter);
     }
 
     }
